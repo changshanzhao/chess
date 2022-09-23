a58ea742ddd2648eaee59a5a743482c79813202b
changshanzhao <104625253+changshanzhao@users.noreply.github.com>
2022-09-23 10:53:39 +0800

    Add files via upload

--- homework1.c
+++ homework1.c
@@ -1,35 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 //我用vs写的，用不惯dev，VS2019不让用scanf，所以有了上面那行,别的编译器可以删了哈
-#define ROW 5
-#define COL 5
-//ROW COL数字可以更改
+/*自动裁判输赢我只写了三子棋的，想扩充的话，我只能想到switch case挨个写一遍的笨方法,
+对于任意的N子琪判定,学长有什么改进意见可以加我qq2395624242*/
+#define ROW 3
+#define COL 3
+//ROW COL数字可以更改,如果更改的话，把IsWin函数注释掉。
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*void hello(){
-	printf("hello world\n");
-}
-void length(){
-	int a = 10;
-	int a_length = sizeof a;
-	printf("int_length=%d\n",a_length);
-	}
-int add(int x,int y){
-	int z = x + y;
-	return z;
-}
-int main(){
-	hello();
-	length();
-	int p;
-	int q;
-	printf("please give me two num\n");
-	scanf("%d%d",&p,&q);
-	int res=add(p,q);
-	printf("%d\n",res);
-	return 0;
-}*/
-//下面才是真正的作业哈哈 N子棋!
 void menu()
 {
 	printf("************************\n");
@@ -75,27 +53,25 @@ void PlayerMove(char board[ROW][COL], int row, int col)
 {
 	int x;
 	int y;
-	printf("玩家走\n");
-	printf("请输入要下的坐标\n");
-	scanf("%d%d", &x, &y);
 	while (1) {
-		if (x >= 1 && x <= row && y >= 1 && y < col)
+		printf("玩家走\n");
+		printf("请输入要下的坐标\n");
+		scanf("%d%d", &x, &y);
+		if (x >= 1 && x <= row && y >= 1 && y <= col)
 		{
-			if (board[x - 1][y - 1] == ' ')
+			if (board[x-1][y-1] == ' ')
 			{
-				board[x - 1][y - 1] = '*';
+				board[x-1][y-1] = '*';
 				break;
 			}
 			else
 			{
 				printf("该处已经落子\n");
-				break;
 			}
 
 		}
 		else {
 			printf("坐标非法，请重新输入!\n");
-			break;
 		}
 			
 	}
@@ -106,22 +82,59 @@ void ComputerMove(char board[ROW][COL], int row, int col)
 	int x;
 	int y;
 	printf("电脑走\n");
-	x = rand() % row;
-	y = rand() % col;
 	while (1) {
+		x = rand() % row;
+		y = rand() % col;
 		if (board[x][y] == ' ')
 		{
 			board[x][y] = '#';
 			break;
 		}
-		
 		}
 	}
-	
+int IsFull(char board[ROW][COL], int row, int col)
+{
+	int i;
+	int j;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			if (board[i][j] == ' ')
+				return 0;
+		}
+	}
+	return 1;
+}
+char IsWin(char board[ROW][COL],int row,int col) {
+	int i;
+	int j;
+	for (i = 0; i < row; i++)
+	{
+		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] != ' ')
+			return board[i][1];
+	}
+	for (j = 0; j < col; j++)
+	{
+		if (board[0][j] == board[1][j] && board[2][j] == board[1][j] && board[1][j] != ' ')
+			return board[1][j];
+	}
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
+		return board[1][1];
+	if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[1][1] != ' ')
+		return board[1][1];
+	if (1 == IsFull(board, ROW, COL))
+	{
+		return 'Q';
+	}
+	return 'C';
+
+}
 
 
 
 void game() {
+	char ret = 0;
 	char board[ROW][COL];
 	//初始化棋盘
 	InitBoard(board, ROW, COL);
@@ -133,14 +146,31 @@ void game() {
 		//玩家下棋
 		PlayerMove(board,ROW,COL);
 		DisplayBoard(board, ROW, COL);
+		ret=IsWin(board,ROW,COL);
+		if (ret != 'C')
+		{
+			break;
+		}
 		//电脑下棋
 		ComputerMove(board,ROW,COL);
 		DisplayBoard(board, ROW, COL);
+		ret=IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
 	}
+	if (ret == '*')
+		printf("玩家赢");
+	else if (ret == '#')
+		printf("电脑赢");
+	else
+		printf("平局");
+
 }
 void test() {
 	int input;
-	srand((unsigned int)time(NULL));
+	srand((unsigned int)time(NULL));//产生随机数
 	do
 	{
 		menu();
@@ -165,5 +195,5 @@ void test() {
 int main() {
 	test();
 
-
+	return 0;
 }
